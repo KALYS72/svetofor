fbc02d31e818ad076e0912c8406963b1f53940e2
kalys <jsvsjsvshsksb@gmail.com>
2023-12-07 15:16:38 +0600

    correction

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ def clock(sec):
     main = time(sec)
     return f"{main['h']:02d}:{main['min']:02d}:{main['sec']:02d}"
 
-def record(filename, object):
+def record(filename, data):
     with open(filename, 'w') as file:
-        for item in object:
+        for item in data:
             file.write(item + '\n')
+
 start = int(input('Write your starting hour: '))
 end = int(input('Write your ending hour: '))
 starting_hour = hours_to_sec(start)
@@ -34,7 +35,7 @@ yellow_iter = []
 red_iter = []
 iterations = 0
 cur_time = starting_hour
-while (time(cur_time))["h"] != 17:
+while (time(cur_time))["h"] != end:
     start_g = cur_time
     cur_time += green_bb
     green_iter.append(clock(start_g) + " - " + clock(cur_time))
@@ -50,5 +51,10 @@ record("green.txt", green_iter)
 record("yellow.txt", yellow_iter)
 record("red.txt", red_iter)
 
+with open("iterations.txt", 'w') as file:
+    file.write(f"Amount of iterations from {start} to {end} is: {iterations}\n")
 
-
+green_iter = []
+yellow_iter = []
+red_iter = []
+iterations = 0
